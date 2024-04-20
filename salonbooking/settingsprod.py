550a9f4339a6e33599dbd4938a951ba923831962
mrmhthegreat <mrmhthegreat@gmail.com>
2024-04-20 20:52:42 +0300

    first commits

--- salonbooking/settingsprod.py
+++ salonbooking/settingsprod.py
@@ -1,4 +1,5 @@
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,7 +10,7 @@ from decouple import config
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY =os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,8 +73,12 @@ WSGI_APPLICATION = 'salonbooking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'glonow',
+        'USER': 'glonowuser',
+        'HOST': 'localhost',
+        'PORT': '',
+        'PASSWORD':os.environ.get("DB_PASSWORD",""),
     }
 }
 
