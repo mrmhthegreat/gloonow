49d1b1fbe5cbe6e0f415b0bbe911d60a319ce415
mrmhthegreat <mrmhthegreat@gmail.com>
2024-05-04 23:58:50 +0300

    m

--- salonbooking/settingsprod.py
+++ salonbooking/settingsprod.py
@@ -135,7 +135,7 @@ LOGIN_URL='singin'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.privateemail.com"
 EMAIL_HOST_USER =config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
